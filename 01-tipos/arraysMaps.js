29a38f474a188bc3f762f2255753f2cfba3b7849
znaiiders <znaiiders@gmail.com>
2026-02-03 10:13:02 -0600

    Metodo MAP

---
+++ 01-tipos/arraysMaps.js
@@ -0,0 +1,7 @@
+let numeros =[1,2,3,4]
+let duplicados = numeros.map(n=> n*2)
+console.log(duplicados)
+
+let nombres =["juan", "maria", "ana"]
+let mayusculas = nombres.map(nombre=>nombre.toUpperCase())
+console.log(mayusculas)
